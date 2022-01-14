c5899a627981dd338f97689a5ea76f03bbaa2ca2
Lucas Esclapez <lucas.esclapez@gmail.com>
2022-01-13 23:26:30 -0800

    Init the convergence machinery.

--- .github/workflows/dependencies/peleDeps.sh
+++ .github/workflows/dependencies/peleDeps.sh
@@ -6,6 +6,5 @@
 # Authors: Lucas Esclapez
 
 echo "Getting PeleLMeX dependencies "
-export TEST_HOME=${PWD}
 mkdir build
 git clone --recursive -b development https://github.com/AMReX-Combustion/PeleLMeX.git build/PeleLMeX
