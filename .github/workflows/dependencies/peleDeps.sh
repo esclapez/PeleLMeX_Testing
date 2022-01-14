9d7c2e91320f1bd05d6130d2da4875b2a9a660f5
Lucas Esclapez <lucas.esclapez@gmail.com>
2022-01-13 22:26:32 -0800

    Initial setup of CI.

---
+++ .github/workflows/dependencies/peleDeps.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#
+# Copyright 2022 The AMReX Community
+#
+# License: BSD-3-Clause-LBNL
+# Authors: Lucas Esclapez
+
+echo "Getting PeleLMeX dependencies "
+export TEST_HOME=${PWD}
+mkdir build
+git clone --recursive -b development https://github.com/AMReX-Combustion/PeleLMeX.git build/PeleLMeX
---
+++ ConvergenceTests/README.md
@@ -0,0 +1,1 @@
+# PeleLMeX ConvergenceTests
